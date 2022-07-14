7d1ef54633cc3b6eb99cd1fd4b2a1f5d6872274f
Frida Fernandez <fridaluzf14@gmail.com>
2022-07-14 11:28:32 -0500

    imagenes previas lista

--- main.js
+++ main.js
@@ -34,7 +34,6 @@ function getElementsModalAddPost() {
   imagesContainerPost = document.querySelector(".uploaded-image");
   buttonCloseModalAddPost = document.querySelector(".close-input");
   spanLocationAddPost = document.querySelector(".location-modal");
-  inputFileImageModalAddPost = document.getElementById("upload-files");
 }
 
 function verifyData() {
@@ -55,15 +54,48 @@ function addEventsElementModalAddPost() {
   buttonLocationPost.addEventListener("click", toggleLocationModalAddPost);
   addEventUploadFileInputModalAddPost();
 }
-
+// image-uploaded
 function addEventUploadFileInputModalAddPost() {
-  console.log(inputFileImageModalAddPost.files);
-  inputFileImageModalAddPost.onchange = () => {
-    console.log("CHANGE");
-    console.log(inputFileImageModalAddPost.files);
-  };
-}
+  'use strict'
+  let file = document.getElementById('upload-files');
+  let formData = new FormData(); 
+  console.log(file);
+  file.addEventListener('change', function(elemento){
+      for(let i = 0 ; i < file.files.length ; i++){
+          let miniature_id = Math.floor(Math.random()*30000) + '_' + Date.now();
+          console.log("dentro del for")
+          createMiniature(file,i,miniature_id);
+          formData.append(miniature_id,file.files[i]);
+          console.log(formData.getElementById);
+      }
+      elemento.target.value = '';
+  });
 
+  let createMiniature = function (file,iterator, miniature_id) {
+      let miniature = document.createElement('div');
+      miniature.classList.add('miniature',miniature_id);
+      miniature.dataset.id = miniature_id;
+      miniature.setAttribute('style',`background-image: url(${ URL.createObjectURL( file.files[iterator] ) })`);
+      document.querySelector('.uploaded-image').appendChild(miniature);
+      createCloseButton(miniature_id);
+  }
+
+  let createCloseButton = function(miniature_id) {
+      console.log("se creo close buton");
+      let closeButton = document.createElement('div');
+      closeButton.classList.add('close-miniature');
+      closeButton.innerText = 'DETELE';
+      document.getElementsByClassName(miniature_id)[0].appendChild(closeButton);
+  }
+
+  document.body.addEventListener('click',function(elemento){
+      if(elemento.target.classList.contains('close-miniature')){
+          elemento.target.parentNode.remove();
+          formData.delete(elemento.target.parentNode.dataset.id);
+      }
+  } )
+}
+// end image-uploaded
 function addEventClickInputPost() {
   inputPost.addEventListener("click", toggleShowAndCloseModalAddPost);
 }
@@ -141,10 +173,6 @@ function assingActionsToPostsButtons() {
   addEventToggleButtonDeleteToButtonsAction();
   addEventShowModalToButtonsDeletes();
   addEventCloseModalToButtonsCloses();
-  addLocation();
-  addModalInput();
-  addBotomsInput();
-  
 
 }
 function init() {
